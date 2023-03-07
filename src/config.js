ef327d5aff23323dfa494b9d67ab692577a1811b
kevin-reyes2525 <78921296+kevin-reyes2525@users.noreply.github.com>
2023-03-07 11:28:21 -0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+config();
+
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017";
+export const PORT = process.env.PORT || 4000;
+export const SECRET = "yoursecretkey";
+
+export const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "admin@localhost";    //opcional
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME || "admin";
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin";
